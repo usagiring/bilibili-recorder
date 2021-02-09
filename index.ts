f337657830984d1ee148f9b130e85b07eea1120e
其妙 <hling51325@163.com>
2021-02-09 20:43:01 +0800

    add download-progress

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import axios, { AxiosInstance, AxiosRequestConfig, CancelTokenSource } from "axios"
+import EventEmitter from "events";
 const CancelToken = axios.CancelToken;
 import * as fs from 'fs'
 
 const BASE_LIVE_URL = 'https://api.live.bilibili.com'
+const DOWNLOAD_TIMER_MS = 2000
 
 interface Options {
   roomId?: number
@@ -14,10 +16,12 @@ interface Options {
 class BilibiliRecorder {
   options: Options
   axiosInstance: AxiosInstance
-  source: CancelTokenSource
+  sourceMap: { [x: string]: CancelTokenSource }
+  emitter: EventEmitter
 
   constructor(options: Options, axiosOptions: AxiosRequestConfig = {}) {
     this.options = options || {}
+    this.emitter = new EventEmitter()
     const defaultAxiosOptions = {
       headers: {
         origin: 'https://live.bilibili.com',
@@ -27,31 +31,59 @@ class BilibiliRecorder {
       },
     }
     this.axiosInstance = axios.create(Object.assign({}, defaultAxiosOptions, axiosOptions))
-    this.source = CancelToken.source();
   }
 
   async record(options: Options) {
+    const now = Date.now()
+    const id = String(now)
+    this.sourceMap = this.sourceMap ? { ...this.sourceMap, id: CancelToken.source() } : { id: CancelToken.source() }
+
     const { roomId, output, qn } = Object.assign({}, this.options, options)
     if (!roomId) throw new Error('roomId is required.')
     const playUrl = await this.getRandomPlayUrl({ roomId, qn })
 
-    const writeStream = fs.createWriteStream(output || `${roomId}_${Date.now()}.flv`);
+    const writeStream = fs.createWriteStream(output || `${roomId}_${now}.flv`);
     const liveStream = await this.getLiveStream(playUrl)
+    const bufferSize = {
+      current: 0,
+      preTick: 0
+    }
+
+    const dowloadTimer = setInterval(() => {
+      const delta = bufferSize.current - bufferSize.preTick
+      const bps = delta / (DOWNLOAD_TIMER_MS / 1000)
+      bufferSize.preTick = bufferSize.current
+      this.emitter.emit(`${id}-download-rate`, { bps, totalSize: bufferSize.current })
+    }, DOWNLOAD_TIMER_MS)
+
     liveStream.on("data", (chunk) => {
+      bufferSize.current = bufferSize.current + chunk.length
       writeStream.write(Buffer.from(chunk));
     });
+
     liveStream.on("end", () => {
       writeStream.end();
+      delete this.sourceMap[id]
+      clearInterval(dowloadTimer)
     });
+
     liveStream.on("error", (e) => {
       console.error(e);
+      writeStream.end();
+      delete this.sourceMap[id]
+      clearInterval(dowloadTimer)
       throw e
     });
+
+    return {
+      id
+    }
   }
 
-  async cancelRecord() {
+  async cancelRecord(id?: string) {
+    id = id || Object.keys(this.sourceMap)[0]
     console.log('cancel live stream')
-    this.source.cancel('Operation canceled by the user.');
+    this.sourceMap[id].cancel('Operation canceled by the user.');
   }
 
   async getPlayUrl(options: Options) {
@@ -67,10 +99,11 @@ class BilibiliRecorder {
     return result.data.durl[Math.floor(Math.random() * urlsLength)].url;
   }
 
-  async getLiveStream(playUrl: string) {
+  async getLiveStream(playUrl: string, id?: string) {
+    id = id || Object.keys(this.sourceMap)[0]
     const res = await this.axiosInstance.get(playUrl, {
       responseType: "stream",
-      cancelToken: this.source.token
+      cancelToken: this.sourceMap[id].token
     })
     return res.data
   }
